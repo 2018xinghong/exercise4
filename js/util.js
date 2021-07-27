879d9b3cb23028dd0ed13fef40824d5065c43d7a
2018xinghong <1784549260@qq.com>
2021-07-27 16:22:06 +0800

    练习4

---
+++ js/util.js
@@ -0,0 +1,28 @@
+// 工具方法
+
+/**
+ * 获取地址栏参数
+*/
+function getURLParam(key) {
+    var paramStr = location.search;
+    var result = null;
+    var obj = {}
+    if(paramStr) {
+        paramStr = paramStr.substr(1).split('&');
+        paramStr.forEach(function(item, index) {
+            var temp = item.split('=');
+            obj[temp[0]] = temp[1];
+        })
+    }
+    if(key) {
+        result = obj[key] ? obj[key] : null;
+        
+    }else {
+        if(JSON.stringify(obj) == '{}') {
+            result = null;
+        }else {
+            result = obj;
+        }
+    }
+    return result;
+}
